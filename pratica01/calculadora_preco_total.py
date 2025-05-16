97992fba73a7b152c79762719d06ad17aac3bc0b
Flavio Serra <sonyflavio@gmail.com>
2025-05-16 20:02:56 -0300

    envio das atividades

---
+++ pratica01/calculadora_preco_total.py
@@ -0,0 +1,5 @@
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade 
+print("Preço total: R$",preco_total)
---
+++ pratica01/calculadora_volume.py
@@ -0,0 +1,7 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print("Volume da caixa:",volume,"cm³")
